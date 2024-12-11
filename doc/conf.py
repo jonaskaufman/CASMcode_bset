a8016fbdd40888252ab242729ea39ef68afc47d3
bpuchala <bpuchala@users.noreply.github.com>
2024-12-11 09:02:53 -0500

    bump version 2.0a2

--- doc/conf.py
+++ doc/conf.py
@@ -2,8 +2,8 @@ import os
 
 # -- package specific configuration --
 project = "casm-bset"
-version = "1.0"  # The short X.Y version.
-release = "2.0a1"  # The full version, including alpha/beta/rc tags.
+version = "2.0"  # The short X.Y version.
+release = "2.0a2"  # The full version, including alpha/beta/rc tags.
 project_desc = "CASM cluster expansion basis set construction module"
 logo_text = "casm-bset"
 github_url = "https://github.com/prisms-center/CASMcode_bset/"
